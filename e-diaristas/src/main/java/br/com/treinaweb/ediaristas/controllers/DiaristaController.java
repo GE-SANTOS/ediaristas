32dce27e50dcb698e366c51f2609240c68077d0a
Geovane Santos <geovane_n.s@outlook.com>
2021-10-15 18:59:17 -0300

    Aplicação web full-stack em Java

---
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
@@ -0,0 +1,46 @@
+package br.com.treinaweb.ediaristas.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import br.com.treinaweb.ediaristas.models.Diarista;
+import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequestMapping("/admin/diaristas")
+public class DiaristaController {
+    
+    @Autowired
+    private DiaristaRepository repository;
+
+    @GetMapping
+    public ModelAndView listar() {
+        var modelAndView = new ModelAndView("admin/diaristas/listar");
+
+        modelAndView.addObject("diaristas", repository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        var modelAndView = new ModelAndView("admin/diaristas/form");
+
+        modelAndView.addObject("diarista", new Diarista());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Diarista diarista) {
+        repository.save(diarista);
+        
+        return "redirect:/admin/diaristas";
+    }
+    
+    
+}
