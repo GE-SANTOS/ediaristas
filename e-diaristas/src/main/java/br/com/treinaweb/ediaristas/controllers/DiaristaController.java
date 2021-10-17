3d81d53f80ea47cb08ad1383a99f3b956f70202f
Geovane Santos <geovane_n.s@outlook.com>
2021-10-17 17:11:45 -0300

    Criar action de listagem de diaristas

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
@@ -1,23 +1,37 @@
 package br.com.treinaweb.ediaristas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.ediaristas.models.Diarista;
+import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class DiaristaController {
 
+    @Autowired
+    private DiaristaRepository repository;
+
     @GetMapping("/cadastrar")
-    public ModelAndView cadatrar(){
+    public ModelAndView cadatrar() {
         var modelAndView = new ModelAndView("admin/diaristas/form");
 
         modelAndView.addObject("diarista", new Diarista());
 
         return modelAndView;
+    }
+    
+    @PostMapping("/cadastrar")
+    public String cadastra(Diarista diarista) {
+        repository.save(diarista);
+
+        return "redirect:/admin/diaristas/cadastrar";
+        
     }
     
 }
