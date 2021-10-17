f6459e01821835d4d1358af23d60e028041db1a8
Geovane Santos <geovane_n.s@outlook.com>
2021-10-17 16:33:05 -0300

    Criar view de formulário de diarista

---
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaController.java
@@ -0,0 +1,23 @@
+package br.com.treinaweb.ediaristas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristas.models.Diarista;
+
+@Controller
+@RequestMapping("/admin/diaristas")
+public class DiaristaController {
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadatrar(){
+        var modelAndView = new ModelAndView("admin/diaristas/form");
+
+        modelAndView.addObject("diarista", new Diarista());
+
+        return modelAndView;
+    }
+    
+}
