ea913a40ce852d36d995e9eaf5db147c047ae5a6
Geovane Santos <geovane_n.s@outlook.com>
2021-10-17 15:34:59 -0300

    Configurar conexão com o Banco de Dados

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diarista.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diarista.java
@@ -6,11 +6,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Diarista {
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
+@EqualsAndHashCode.Include
 private Long id;
 
 @Column(nullable = false, length = 100)
