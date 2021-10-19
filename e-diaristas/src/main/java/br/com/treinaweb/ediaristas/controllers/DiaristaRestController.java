4ccc6d36ab7f3c7bedcb20d7a8c188b2730d3c00
Geovane Santos <geovane_n.s@outlook.com>
2021-10-19 14:29:12 -0300

    Paginar o resultado da consulta de diaristas

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
@@ -1,14 +1,13 @@
 package br.com.treinaweb.ediaristas.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.treinaweb.ediaristas.models.Diarista;
+import br.com.treinaweb.ediaristas.dtos.DiaristasPagedResponse;
 import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
 import br.com.treinaweb.ediaristas.services.ViaCepService;
 
@@ -23,11 +22,16 @@ public class DiaristaRestController {
     private ViaCepService viaCepService;
 
     @GetMapping
-    public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep) {
+    public DiaristasPagedResponse buscarDiaristasPorCep(@RequestParam String cep) {
         var endereco = viaCepService.buscarEnderecoPorCep(cep);
         var codigoIbge = endereco.getIbge();
 
-        return repository.findByCodigoIbge(codigoIbge);
+        var pageable = PageRequest.of(0, 6);
+        var diaristas = repository.findByCodigoIbge(codigoIbge, pageable);
+
+        var quantidadeDiaristas = diaristas.getTotalElements() > 6 ? diaristas.getTotalElements() - 6 : 0;
+
+        return new DiaristasPagedResponse(diaristas.getContent(), quantidadeDiaristas);
     }
     
 }
