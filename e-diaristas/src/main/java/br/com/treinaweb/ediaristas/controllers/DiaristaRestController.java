68dd397b4e768313a6595e1b1a54fbc8168a876c
Geovane Santos <geovane_n.s@outlook.com>
2021-10-19 11:25:21 -0300

    Filtrar diaristas com base no código IBGE

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
+++ e-diaristas/src/main/java/br/com/treinaweb/ediaristas/controllers/DiaristaRestController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.ediaristas.models.Diarista;
 import br.com.treinaweb.ediaristas.repositories.DiaristaRepository;
+import br.com.treinaweb.ediaristas.services.ViaCepService;
 
 @RestController
 @RequestMapping("/api/diaristas-cidade")
@@ -17,9 +19,15 @@ public class DiaristaRestController {
     @Autowired
     private DiaristaRepository repository;
 
+    @Autowired
+    private ViaCepService viaCepService;
+
     @GetMapping
-    public List<Diarista> buscarDiaristasPorCep() {
-        return repository.findAll();
+    public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep) {
+        var endereco = viaCepService.buscarEnderecoPorCep(cep);
+        var codigoIbge = endereco.getIbge();
+
+        return repository.findByCodigoIbge(codigoIbge);
     }
     
 }
