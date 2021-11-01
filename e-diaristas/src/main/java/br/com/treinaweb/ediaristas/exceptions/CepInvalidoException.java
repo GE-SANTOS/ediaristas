f54a8140af1a0d9c29006e902db481ad3825cd70
Geovane Santos <geovane_n.s@outlook.com>
2021-11-01 10:26:46 -0300

    Atualização

--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/exceptions/CepInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package br.com.treinaweb.ediaristas.exceptions;
-
-public class CepInvalidoException extends RuntimeException {
-    
-    public CepInvalidoException(String message) {
-        super(message);
-    }
-    
-}
--- e-diaristas/src/main/java/br/com/treinaweb/ediaristas/exceptions/CepNaoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package br.com.treinaweb.ediaristas.exceptions;
-
-public class CepNaoEncontradoException extends RuntimeException {
-
-    public CepNaoEncontradoException(String message) {
-        super(message);
-    }
-    
-}
